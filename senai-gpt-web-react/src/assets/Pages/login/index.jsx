5a98c0d6885ae42f6b8140069a24b6359105115e
daniel <danielgomes5039@email.com>
2025-04-15 21:52:01 -0300

    ...

--- senai-gpt-web-react/src/assets/Pages/login/index.jsx
+++ senai-gpt-web-react/src/assets/Pages/login/index.jsx
@@ -3,7 +3,29 @@ function Login() {
   
     return (
        <>
-         <h1>Login</h1>
+             <header></header>
+
+<main className="page-container">
+
+    <div className="robo-imagen">
+
+    </div>
+
+    <div className="login_container">
+        <img className="logo" src="../assets/imgs/Chat.png" alt="logo SenaiGPT"/>
+        {/* <!-- colocar titulo; "style= estilo da fonte (tamnaho..)"  --> */}
+        <h1 id="meutitulo" className="titulo">Login
+        </h1>
+
+        {/* <!-- imput: colocar caixa de dialogo --> */}
+        {/* <!-- placeholder : letrinha q fica no fundo--> */}
+        <input className="input" type="email" placeholder="insira o e-mail"/>
+        <input className="input" type="password" placeholder="insira a senha"/>
+        <button className="btn">Entrar</button>
+    </div>
+
+</main>
+<footer></footer>
        </>
      )
    }
